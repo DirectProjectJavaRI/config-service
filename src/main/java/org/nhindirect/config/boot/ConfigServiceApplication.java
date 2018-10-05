68e242edac642fcf0dd904165d601199c7a5754e
gm2552 <gm2552@localhost>
2018-10-05 09:19:40 -0500

    Updating dependencies:
    - Updating spring boot dependencies to 2.1.0.M4
    - Updating spring cloud dependencies to Greenwich.M1

--- src/main/java/org/nhindirect/config/boot/ConfigServiceApplication.java
+++ src/main/java/org/nhindirect/config/boot/ConfigServiceApplication.java
@@ -4,13 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 
-@ComponentScan(basePackages = { "org.nhindirect.config.springconfig", "org.nhindirect.config.boot", 
-		"org.nhindirect.config.store", "org.nhindirect.config.resources"})
+@ComponentScan({"org.nhindirect.config"})
 @SpringBootApplication
-@Configuration
 @EnableEurekaClient
 public class ConfigServiceApplication
 {	
