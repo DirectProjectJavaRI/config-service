d30d1fbfdcc74b857656fce22ece80fe40c86975
gm2552 <gm2552@xmppserver.cernerdirectsupport.com>
2021-07-28 14:57:15 -0500

    Initial Direct RI 8.0.0 checkin.

--- src/main/java/org/nhindirect/config/boot/ConfigServiceApplication.java
+++ src/main/java/org/nhindirect/config/boot/ConfigServiceApplication.java
@@ -1,19 +1,24 @@
 package org.nhindirect.config.boot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @ComponentScan({"org.nhindirect.config"})
-@SpringBootApplication
-@EnableEurekaClient
+@SpringBootApplication()
+@EnableR2dbcRepositories("org.nhindirect.config.repository")
+@EnableScheduling
 public class ConfigServiceApplication
 {	
     public static void main(String[] args) 
     {
-        SpringApplication.run(ConfigServiceApplication.class, args);
+    	final SpringApplication app =  new SpringApplication(ConfigServiceApplication.class);
+    	app.setWebApplicationType(WebApplicationType.REACTIVE);
+    	app.run(args);
     }  
 
 }
--- src/main/java/org/nhindirect/config/boot/WebSecurityConfiguration.java
+++ src/main/java/org/nhindirect/config/boot/WebSecurityConfiguration.java
@@ -1,19 +1,32 @@
 package org.nhindirect.config.boot;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
-@EnableWebFluxSecurity
-public class WebSecurityConfiguration
+@Configuration
+public class WebSecurityConfiguration 
 {
 	@Bean
 	public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http)
 	{
-		http.csrf().disable();
 
-	    return http.build(); 
+		http.csrf().disable()
+        .authorizeExchange()
+        .pathMatchers("/domains/**", "/address/**", "/anchor/**", 
+        		"/certificate/**", "/certpolicy/**", "/dns/**", 
+        		"/setting/**", "/trustbundle/**").permitAll()
+        .anyExchange().authenticated()
+        .and()
+        .httpBasic()
+        .and()
+        .formLogin().disable();
+
+
+		
+	    return http.build();
 	}
-}
 
+		      
+}
