3743a4ec4f99bab2e08a11b0b377dc2440591484
greg.meyer <greg.meyer@oracle.com>
2025-08-07 14:49:09 -0500

    Initial RI 9.0 commit.

--- src/main/java/org/nhindirect/config/boot/ConfigServiceApplication.java
+++ src/main/java/org/nhindirect/config/boot/ConfigServiceApplication.java
@@ -3,14 +3,10 @@ package org.nhindirect.config.boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
-@ComponentScan({"org.nhindirect.config"})
 @SpringBootApplication()
-@EnableR2dbcRepositories("org.nhindirect.config.repository")
 @EnableScheduling
 public class ConfigServiceApplication
 {	
