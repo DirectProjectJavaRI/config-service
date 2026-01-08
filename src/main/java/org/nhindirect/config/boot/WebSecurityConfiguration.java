c08fd9c8d2b9a1c6c8d53691d66fceff891376dd
Greg Meyer <abareasol@gmail.com>
2026-01-08 07:20:33 -0600

    Adding basic auth via java bean vs spring property.

--- src/main/java/org/nhindirect/config/boot/WebSecurityConfiguration.java
+++ src/main/java/org/nhindirect/config/boot/WebSecurityConfiguration.java
@@ -2,6 +2,7 @@ package org.nhindirect.config.boot;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
@@ -28,6 +29,7 @@ public class WebSecurityConfiguration
 	        ).authenticated()
 	        .anyExchange().permitAll());
 
+       http.httpBasic(Customizer.withDefaults());
 		
 	    return http.build();
 	}
